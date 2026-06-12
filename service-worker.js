a9cb81349b575fd50b941abd32628ae725380f7c
titir95biplab-dotcom <titir95biplab@gmail.com>
2026-06-12 19:06:18 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -29,7 +29,7 @@ messaging.onBackgroundMessage((payload) => {
 
 
 // ── PWA CACHING ──────────────────────────────────────────
-const CACHE_NAME = 'jt-majherhati-pos-v7';
+const CACHE_NAME = 'jt-majherhati-pos-v8';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
